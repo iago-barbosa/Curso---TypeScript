3b3a0c7a2b8e42a70f33a1fef8c1c1789a30f0c9
Iago Barbosa <gliago_iago@hotmail.com>
2021-04-05 10:25:51 -0300

    2021-04-05 final do modulo 4 e modulo 5 finalizando o curso

--- TypeScript_1/alurabank/app/ts/views/View.ts
+++ TypeScript_1/alurabank/app/ts/views/View.ts
@@ -1,9 +1,17 @@
-class View {
+abstract class View<T> {
 
-    protected _elemento: Element;
+    protected _elemento: JQuery;
 
     constructor (seletor: string) {
 
-        this._elemento = document.querySelector(seletor);
+        this._elemento = $(seletor);
     }
+
+    update(model: T) {
+
+        this._elemento.html(this.template(model));
+    }
+
+    abstract template(model: T): string;
+
 }
